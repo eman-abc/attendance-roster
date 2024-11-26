6d5b22f81fc6191e85e497d31a63b13abf0856c0
asmasaeed18as <133525373+asmasaeed18as@users.noreply.github.com>
2024-11-26 14:48:10 +0500

    Create script.js to use javascript

---
+++ student/script.js
@@ -0,0 +1,4 @@
+// Simple script to dynamically add interactivity if needed
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Student attendance page loaded successfully.');
+});
